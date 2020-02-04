d4d7f1c2b9c510de473a0b9ff41273222f533f42
Chams91 <sunnychamsever@gmail.com>
2020-02-04 16:23:40 +0100

    my final magical touches

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
-import (  
-		  "fmt"
-		  "log"
-		  "net/http"
-		 )
-var version =1
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var version = 3
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	        fmt.Fprintln(w, "Hello, 世界.. From V %s", version ) }
+	fmt.Fprintln(w, "Hello, 世界.. From v %s", version)
+}
 func main() {
-			http.HandleFunc("/", handler)
-			log.Fatal(http.ListenAndServe(":8888", nil)) 
-			} 
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8888", nil))
+}
